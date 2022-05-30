e564d37354f9e29dab127a7543493065908f8a2f
Olga Tsybikova <tsy2701@gmail.com>
2022-05-30 15:57:49 +0200

    Added Eslint and config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
