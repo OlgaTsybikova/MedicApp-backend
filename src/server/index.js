e01d5f32436be3c1cc6f4b87f41bc8b4fb01362b
Olga Tsybikova <tsy2701@gmail.com>
2022-05-30 22:26:37 +0200

    Added cors for front

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
+const corsOptions = require("../utils/corsOptions");
+const { notFoundError, generalError } = require("./middlewares/errors/errors");
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
+app.use(helmet());
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
+
 module.exports = app;
