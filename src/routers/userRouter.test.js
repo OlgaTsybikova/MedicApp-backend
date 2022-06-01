d8633131cd7644ae00f931127ce8969fdcdc8dc7
Olga Tsybikova <tsy2701@gmail.com>
2022-06-01 13:46:49 +0200

    Tested User login router

--- src/routers/userRouter.test.js
+++ src/routers/userRouter.test.js
@@ -1,7 +1,9 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const request = require("supertest");
 const connectDB = require("../database");
+const User = require("../database/models/User");
+const mockNewUser = require("../mocks/usersMock");
 const app = require("../server");
 
 let mongoServer;
@@ -10,16 +12,40 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   await connectDB(mongoServer.getUri());
 });
+beforeEach(async () => {
+  await User.create(mockNewUser[0]);
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
   await mongoServer.stop();
 });
+
 describe("Given a POST/user/register/ endpoint", () => {
   describe("When it receives a request with a new user", () => {
     test("Then it should respond with the status code 201 and the registered user", async () => {
       const newUser = { username: "luisito", password: "luisito" };
       await request(app).post("/user/register").send(newUser).expect(201);
+    });
+  });
+});
+
+describe("Given a POST/user/login endpoint", () => {
+  describe("When it receives a request with an existing user", () => {
+    test("Then it should respond with the status code 200 and a token", async () => {
+      const user = {
+        username: "admin",
+        password: "admin",
+      };
+
+      const {
+        body: { token },
+      } = await request(app).post("/user/login").send(user).expect(200);
+      expect(token).not.toBeNull();
     });
   });
 });
