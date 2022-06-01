4cc9865958f0a037036593514a5d47dcf07e47e0
Olga Tsybikova <tsy2701@gmail.com>
2022-06-01 12:29:09 +0200

    Created login user controller , router and tested the controller

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../database/models/User");
 
 const userRegister = async (req, res, next) => {
@@ -27,6 +28,37 @@ const userRegister = async (req, res, next) => {
   }
 };
 
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("There is no user with this name...");
+    error.statusCode = 403;
+
+    next(error);
+  } else {
+    const matchingPassword = bcrypt.compare(password, user.password);
+
+    const userData = {
+      username: user.username,
+      password: user.password,
+      id: user.id,
+    };
+
+    if (!matchingPassword) {
+      const error = new Error("Password is wrong...Please, try again...");
+      error.code = 403;
+      error.customError = "Oops, can't let you in with theis info...";
+      next(error);
+    } else {
+      const token = jwt.sign(userData, process.env.JWT_SECRET);
+      res.status(200).json(token);
+    }
+  }
+};
+
 module.exports = {
   userRegister,
+  userLogin,
 };
