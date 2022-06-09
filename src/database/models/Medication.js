e57c24b02abc9fce076ee20d42d66ea2608d1053
Olga Tsybikova <tsy2701@gmail.com>
2022-06-09 18:53:07 +0200

    Changed controller to load images

--- src/database/models/Medication.js
+++ src/database/models/Medication.js
@@ -3,10 +3,10 @@ const { Schema, model, SchemaTypes } = require("mongoose");
 const medicationSchema = new Schema({
   title: { type: String, required: true },
   image: String,
-  category: [],
+  category: String,
   prospect: String,
-  description: { type: [String] },
-  uses: { type: [String] },
+  description: String,
+  uses: String,
   dosis: String,
   owner: { type: SchemaTypes.ObjectId, ref: "users" },
   treatment: { type: String },
