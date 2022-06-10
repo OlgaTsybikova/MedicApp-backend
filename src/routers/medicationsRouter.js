ff79279092a7322306f9b1ed3d7775e675f04a83
Olga Tsybikova <tsy2701@gmail.com>
2022-06-10 15:11:16 +0200

    Created controller and router de update

--- src/routers/medicationsRouter.js
+++ src/routers/medicationsRouter.js
@@ -5,6 +5,7 @@ const {
   getMedications,
   deleteMedications,
   createMedication,
+  updateMedication,
 } = require("../controllers/medicationsControllers");
 
 const upload = multer({ dest: "images/" });
@@ -13,4 +14,5 @@ const medicationsRouter = express.Router();
 medicationsRouter.get("/list", getMedications);
 medicationsRouter.delete("/:id", deleteMedications);
 medicationsRouter.post("/create", upload.single("image"), createMedication);
+medicationsRouter.put("/update/:id", upload.single("image"), updateMedication);
 module.exports = medicationsRouter;
